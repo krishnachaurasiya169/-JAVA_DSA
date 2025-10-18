b286ff7f975e5c9b90f6e311180dbafca7365837
Krishna2300320120132 <krishnafamily32@gmail.com>
2025-10-18 14:38:15 +0530

    ticket route iternity

--- src/ticketkaroute.java
+++ src/ticketkaroute.java
@@ -1,23 +1,25 @@
 import java.util.*;
-public class ticketkaroute {
+public class ticketkaroute
+{
 
-    public static String getStart(HashMap<String,String> tick){
-        HashMap<String,String> revMap=new HashMap<>();
-//      aab key a set nikal loo
-        for(String key: tick.keySet()){
-        String value=tick.get(key);
-        revMap.put(value,key);
+    public static String getStart(HashMap<String,String> tick) {
+
+        HashMap<String, String> revMap = new HashMap<>();
+
+        for (String key : tick.keySet())
+        {
+            String value = tick.get(key);
+            revMap.put(value, key);
         }
-        for(String key: tick.keySet()){
-//            yadi key nahi mila to wahi hmra start
-            if(! revMap.containsKey(key)){
+        for (String key : tick.keySet())
+        {
+            if (!revMap.containsKey(key))
+            {
                 return key;
             }
         }
         return null;
     }
-
-
     public static void main(String[] args)
     {
         HashMap<String,String> orignal=new HashMap<>();
@@ -28,12 +30,11 @@ public class ticketkaroute {
 // ab tick me orignal wale hashmap data store ho gya hai
 //        ab jo bhi hai wo tick me operation perform hoga
         String start=getStart(orignal);
-        while(orignal.containsKey(start)){
+        while (orignal.containsKey(start))
+        {
             System.out.print(start+"->");
-            start=orignal.get(start);
-
+            start= orignal.get(start);
         }
         System.out.println(start);
-
     }
 }
