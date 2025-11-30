6b47eb7b58eadc99f3d7a94e24d15197fc229a61
Krishna2300320120132 <krishnafamily32@gmail.com>
2025-11-30 15:23:09 +0530

    function in dsa passing  parameter

--- src/function/sum.java
+++ src/function/sum.java
@@ -3,7 +3,20 @@ package function;
 import java.util.Scanner;
 
 public class sum {
-     public void sum(){
+    public static void main(String[] args) {
+
+        System.out.println( sum3(23,32));
+    }
+//pass the value of number when yoy are calling the method in main()
+
+
+
+    static int sum3(int m,int n){
+        int ans1=m+n;
+        return ans1;
+    }
+
+     public static void sum(){
          Scanner sc=new Scanner(System.in);
          int a,b,sum;
          System.out.println("enter the number 1 : ");
@@ -13,8 +26,5 @@ public class sum {
          sum=a+b;
          System.out.println("the sum is.."+sum);
      }
-    public static void main(String[] args) {
-      sum s1=new sum();
-      s1.sum();
-    }
+
 }
