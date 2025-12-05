ebe56773c18a26dca24e649413dc831893c287cf
krishna2300320120132 <krishnafamily32@gmail.com>
2025-12-05 15:04:19 +0530

    reverse the element in an array

---
+++ src/array/reverseelement.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.Arrays;
+
+public class reverseelement {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 4, 23, 18};
+//        swap(arr,0,arr.length);
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void reverse(int[]arr){
+        int start=0;
+        int end= arr.length-1;
+       while(start<end){
+           swap(arr,start,end);
+           start++;
+           end--;
+       }
+
+    }
+
+    static void swap(int[] arr,int index1,int index2){
+        int temp=arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+    }
+
+}
