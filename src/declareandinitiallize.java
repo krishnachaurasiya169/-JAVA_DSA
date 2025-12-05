8d85860b16cfa37bf15c988819414bfb69d449d9
krishna2300320120132 <krishnafamily32@gmail.com>
2025-12-05 12:21:43 +0530

    declare and initiallize the array. if we creating the obj in an array then array gather the size in heap memory

---
+++ src/declareandinitiallize.java
@@ -0,0 +1,21 @@
+public class declareandinitiallize {
+    public static void main(String[] args) {
+
+//        syntax  of declare array
+//         datatype[] variable_name = new datatype[size];
+
+
+
+
+//      declare the array
+
+        int[] arr;
+
+
+//        initiallize the arr size if we can't put/mention the size then it will give you the error;
+
+        arr=new int[12];
+
+
+    }
+}
