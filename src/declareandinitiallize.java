e1f9e499d8fc381cc2b7d1d72fdb16129b789d50
krishna2300320120132 <krishnafamily32@gmail.com>
2025-12-05 12:33:25 +0530

    declare and initiallize the array. if we creating the obj in an array then array gather the size in heap memory

--- src/declareandinitiallize.java
+++ src/declareandinitiallize.java
@@ -8,14 +8,17 @@ public class declareandinitiallize {
 
 
 //      declare the array
-
         int[] arr;
-
 
 //        initiallize the arr size if we can't put/mention the size then it will give you the error;
 
         arr=new int[12];
+//      {0,0,0,0,0,0,0,0,0,0,0,0} ye 12 size ka array aur ye empty hai means 0 hai each index pr;
+        System.out.println(arr[0]);
 
+String[] str;
+str=new String[5];
+        System.out.println(str[2]);
 
     }
 }
