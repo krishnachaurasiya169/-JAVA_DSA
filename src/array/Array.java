70db401245fa4ca1caacd67f76a0eaba2f74d795
krishna2300320120132 <krishnafamily32@gmail.com>
2025-12-05 13:09:56 +0530

    arrayinput

---
+++ src/array/Array.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+//        int [] arr=new int[5];
+//
+//        for(int i=0;i<arr.length;i++){
+//            arr[i]=in.nextInt();
+//        }
+//        for(int i=0;i<arr.length;i++){
+//            System.out.print(arr[i]+" ");
+//        }
+//
+//        for(int num : arr){
+//            System.out.println(num+" ");
+//        }
+
+        String[] str=new String[3];
+
+        for(int i=0;i<str.length;i++){
+            str[i]=in.next();
+        }
+        System.out.println(Arrays.toString(str));
+
+//        modify
+        str[1]="krishna";
+    }
+}
