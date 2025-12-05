1a388706a1b4fc05db4936f9c6a3875d566b9cc5
krishna2300320120132 <krishnafamily32@gmail.com>
2025-12-05 13:10:41 +0530

    taking arrayinput

--- src/array/Arrayinput.java
+++ src/array/Arrayinput.java
@@ -3,7 +3,7 @@ package array;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Array {
+public class Arrayinput {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
 //        int [] arr=new int[5];
